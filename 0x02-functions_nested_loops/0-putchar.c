1b9728f3e7b14ca0ae057a7d101437f867205109
Leviathan72 <jfrancoiszouba72@gmail.com>
2023-02-22 03:51:04 +0100

    Update 0-puchar.c + remove _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
- * main - Main program function
- * Return: 0 if no problem appear
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	char text[8] = "_putchar";
+	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 8; i++)
+	{
 		_putchar(text[i]);
+	}
 	_putchar('\n');
 
 	return (0);
