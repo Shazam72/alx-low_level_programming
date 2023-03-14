d5a95801f10f9fc82b26d185ff536673bb4881c0
leviathan72 <jfrancoiszouba72@gmail.com>
2023-03-14 20:14:48 +0100

    Adding mandatory source files

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function
+ * @str: size of the array
+ * Return: a pointer to the newly allocated string
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+	new_str = malloc(sizeof(char) * strlen(str));
+	if (new_str == NULL)
+		return (NULL);
+	strcpy(new_str, str);
+
+	return (new_str);
+}
