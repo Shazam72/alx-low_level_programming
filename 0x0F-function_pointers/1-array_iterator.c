4b56a67dd82eaf068a748291be7f5bbf4684a71a
leviathan72 <jfrancoiszouba72@gmail.com>
2023-03-22 18:56:35 +0100

    Adding second task source files: correct size_t for unsigned in

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,20 @@
-#include <stddef.h>
 #include <stdlib.h>
+#include "function_pointers.h"
+
 /**
- * array_iterator - function given as a parameter on each element of an array
- * @array: array to use
- * @size: size of array
- * @action: pointer to the function to use
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to function used
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (!action || size == 0)
+	if (!array || !action)
 		return;
 
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
