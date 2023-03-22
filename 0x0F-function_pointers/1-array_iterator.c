b38d738c2bb691dc8dc8d6a6fe7109ed883f88fc
leviathan72 <jfrancoiszouba72@gmail.com>
2023-03-22 18:49:16 +0100

    Adding second task source files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * array_iterator - function given as a parameter on each element of an array
+ * @array: array to use
+ * @size: size of array
+ * @action: pointer to the function to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (!action || size == 0)
+		return;
+
+	for (; i < size; i++)
+		action(array[i]);
+}
