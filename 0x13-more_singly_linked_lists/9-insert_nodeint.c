f245c9b7dcb7ef91b1429e7c7ce481e09ac16506
leviathan72 <jfrancoiszouba72@gmail.com>
2023-04-05 12:00:30 +0200

    Update tasks 9 source file!

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,25 +12,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *prev, *new;
 
-	if (head == NULL)
+	if (head == NULL || idx == 0)
 	{
 		new = malloc(sizeof(*new));
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
+		if (head != NULL)
+		{
+			prev = *head;
+			new->next = prev;
+			*head = new;
+		}
 		return (new);
 	}
 	prev = *head;
-	if (idx == 0)
-	{
-		new = malloc(sizeof(*new));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		new->next = prev;
-		*head = new;
-		return (new);
-	}
 	for (i = 0; i < idx - 1 && prev != NULL; i++)
 		prev = prev->next;
 	if (i == idx - 1)
