4e4b4dbb25f0058ad0f444755e9dacb35715ee0b
leviathan72 <jfrancoiszouba72@gmail.com>
2023-04-05 11:05:08 +0200

    Update tasks 9 source files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,9 +12,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *prev = *head, *new;
 
-	if (prev == NULL || head == NULL)
+	if (prev == NULL)
 		return (NULL);
-	if (idx == 0)
+	if (idx == 0 || head == NULL)
 	{
 		new = malloc(sizeof(*new));
 		if (new == NULL)
--- 0x13-more_singly_linked_lists/9-main.c
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -14,17 +14,8 @@ int main(void)
 	void *pt;
 
 	head = NULL;
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
 	printf("-----------------\n");
-	pt = insert_nodeint_at_index(&head, -12, 4096);
+	pt = insert_nodeint_at_index(NULL, -12, 4096);
 	printf("Nous eumes %p", pt);
 	print_listint(head);
 	free_listint2(&head);
