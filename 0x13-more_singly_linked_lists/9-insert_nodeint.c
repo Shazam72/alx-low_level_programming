ad4b25fef6c25d2a6ec0689bb400a7cd07ba4763
leviathan72 <jfrancoiszouba72@gmail.com>
2023-04-05 10:58:07 +0200

    Update tasks 9 source files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *prev = *head, *new;
 
-	if (prev == NULL)
+	if (prev == NULL || head == NULL)
 		return (NULL);
 	if (idx == 0)
 	{
